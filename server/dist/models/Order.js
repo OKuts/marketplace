6add40301eab8df5e89ced53b356a25d71301b10
OKuts <kuts.ol.vas@gmail.com>
2023-02-27 07:33:23 +0200

    delete dist

--- server/dist/models/Order.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Order = void 0;
-const mongoose_1 = require("mongoose");
-const orderSchema = new mongoose_1.Schema({
-    name: { type: String, require: true },
-    phone: { type: Number, require: true }
-});
-exports.Order = (0, mongoose_1.model)('Order', orderSchema);
